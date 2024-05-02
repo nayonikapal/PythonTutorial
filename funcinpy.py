4169b2e6dc069a6b54e7aae8c944f243443d10cd
ujjwal <ujjwal.srivastava@laitkor.com>
2024-05-02 17:59:20 +0530

    teached functions &  their parameters, values

---
+++ funcinpy.py
@@ -0,0 +1,37 @@
+# def showName(mob_f_5, mob_l_5,mob_g_5):
+#     print(mob_f_5,mob_l_5,mob_g_5)
+
+
+# showName("22334","67890","12345",)
+# showName(mob_g_5="22334",mob_l_5="67890",mob_f_5="12345",)
+# def my_function(age=10, name="Noname"):
+#     print("Age Of " + name + " is " + str(age))
+
+
+# my_function()
+
+
+# def multply(n):
+#     pass
+    # sq = n**2
+    # dbl = n+n
+    # tripl = n+n+n
+    # print(sq,dbl,tripl)
+    # return sq,dbl,tripl
+
+# m = multply(3)
+# print(m)
+
+
+
+
+# print("My Name Is Rajesh")
+# print("My Name Is Priyanka")
+# print("My Name Is Raju")
+
+
+
+def my_function(*,x,y,z,):
+  print(x,y,z)
+# my_function(3,4,5)
+my_function(3,4,5)
