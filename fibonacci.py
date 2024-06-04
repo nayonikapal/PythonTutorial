ebbf2be43e806ad45010f0d184e471c2ff96c13a
Nayonika Pal <nayonika.pal@laitkor.com>
2024-06-04 10:09:59 +0530

    resolving conflicts

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-x=int(input("Enter the length of numbers in the sequence: "))
 
-if x<0:
-    print("Invalid input")
-yf=0
-ys=1
-print(yf,ys,end=" ")
-for i in range(2,x):
-    r=yf+ys
-    print(r,end=" ")
-    yf=ys
-    ys=r
-=======
 # 0 1 1 2 3 5 8 13 21
 
 num = int(input("Enter the number you wnt to print fibonacci series : "))
@@ -24,4 +11,3 @@ for i in range(num):
     first = second
     second = res
     
->>>>>>> dbc758829c6d1953461c2c63eafbe155c4d481b3
