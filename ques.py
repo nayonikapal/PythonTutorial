37407305a18f027ce2f6adb1094cd67984dabf2a
Nayonika Pal <nayonika.pal@laitkor.com>
2024-07-08 12:21:23 +0530

    completed questions

--- ques.py
+++ ques.py
@@ -11,218 +11,218 @@
 
 '''QUESTION 1'''
 
-# class Shape:
+class Shape:
     
-#     def area(self):
-#         pass
+    def area(self):
+        pass
 
-#     def peri(self):
-#         pass
+    def peri(self):
+        pass
 
 
-# class Rectangle(Shape):
+class Rectangle(Shape):
 
-#     def __init__(self,l,br):
-#         self.l=l
-#         self.br=br
+    def __init__(self,l,br):
+        self.l=l
+        self.br=br
 
-#     def area(self):
-#         print("Area of rectangle is")
-#         return self.l*self.br
+    def area(self):
+        print("Area of rectangle is")
+        return self.l*self.br
     
-#     def peri(self):
-#         print("Perimeter of rectangle is")
-#         return 2*(self.l+self.br)
+    def peri(self):
+        print("Perimeter of rectangle is")
+        return 2*(self.l+self.br)
     
-# class Circle(Shape):
+class Circle(Shape):
 
-#     def __init__(self,r):
-#         self.r=r
+    def __init__(self,r):
+        self.r=r
     
-#     def area(self):
-#         print("Area of a circle is")
-#         return 3.14*self.r*self.r
+    def area(self):
+        print("Area of a circle is")
+        return 3.14*self.r*self.r
     
-#     def peri(self):
-#         print("Perimeter of a circle is")
-#         return 2*3.14*self.r
+    def peri(self):
+        print("Perimeter of a circle is")
+        return 2*3.14*self.r
     
-# class Triangle(Shape):
+class Triangle(Shape):
 
-#     def __init__(self,a,b,c,bt,h):
-#         self.a=a
-#         self.b=b
-#         self.c=c
-#         self.bt=bt
-#         self.h=h
+    def __init__(self,a,b,c,bt,h):
+        self.a=a
+        self.b=b
+        self.c=c
+        self.bt=bt
+        self.h=h
 
-#     def area(self):
-#         print("Area of a triangle is")
-#         return 0.5*self.bt*self.h
+    def area(self):
+        print("Area of a triangle is")
+        return 0.5*self.bt*self.h
     
-#     def peri(self):
-#         print("Perimeter of a triangle is")
-#         return self.a+self.b+self.c
+    def peri(self):
+        print("Perimeter of a triangle is")
+        return self.a+self.b+self.c
 
 
-# def main():
+def main():
 
-#     print('''Calculate area and perimeter of rectangle, circle and triangle
-#                 Which shape?
-#                     1. Rectangle
-#                     2. Circle
-#                     3. Triangle
-#                     4. Exit''')
+    print('''Calculate area and perimeter of rectangle, circle and triangle
+                Which shape?
+                    1. Rectangle
+                    2. Circle
+                    3. Triangle
+                    4. Exit''')
 
-#     i=int(input("->"))
+    i=int(input("->"))
 
-#     if i==1:
-#         l=float(input("Enter the length: "))
-#         br=float(input("Enter the breadth: "))
+    if i==1:
+        l=float(input("Enter the length: "))
+        br=float(input("Enter the breadth: "))
 
-#         shape=Rectangle(l,br)
+        shape=Rectangle(l,br)
 
-#     elif i==2:
-#         r=float(input("Enter the radius: "))
+    elif i==2:
+        r=float(input("Enter the radius: "))
 
-#         shape=Circle(r)
+        shape=Circle(r)
 
-#     elif i==3:
-#         a=float(input("Enter value of side 'a': "))
-#         b=float(input("Enter value of side 'b': "))
-#         c=float(input("Enter value of side 'c': "))
-#         bt=float(input("Enter the base of the triangle: "))
-#         h=float(input("Enter the height of the triangle: "))
+    elif i==3:
+        a=float(input("Enter value of side 'a': "))
+        b=float(input("Enter value of side 'b': "))
+        c=float(input("Enter value of side 'c': "))
+        bt=float(input("Enter the base of the triangle: "))
+        h=float(input("Enter the height of the triangle: "))
 
-#         shape=Triangle(a,b,c,bt,h)
+        shape=Triangle(a,b,c,bt,h)
 
-#     elif i==4:
-#         exit()
+    elif i==4:
+        exit()
 
-#     else:
-#         print("Invalid input")
+    else:
+        print("Invalid input")
 
-#     print('''What would you like to calculate?
-#                 1. Area
-#                 2. Perimeter''')
+    print('''What would you like to calculate?
+                1. Area
+                2. Perimeter''')
     
-#     j=int(input("->"))
+    j=int(input("->"))
 
-#     if j==1:
-#         print(f"The area is: {shape.area()}")
+    if j==1:
+        print(f"The area is: {shape.area()}")
 
-#     if j==2:
-#         print(f"The perimeter is: {shape.peri()}")
+    if j==2:
+        print(f"The perimeter is: {shape.peri()}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
 '''QUESTION 2'''
 
-# class Account:
+class Account:
 
-#     def __init__(self,ACCOUNT_ID,BANKNAME,NAME_ON_ACC,BRANCH,BALANCE=200.00):
-#         self.account_id=ACCOUNT_ID
-#         self.bankname=BANKNAME
-#         self.name_on_acc=NAME_ON_ACC
-#         self.branch=BRANCH
-#         self.balance=BALANCE
+    def __init__(self,ACCOUNT_ID,BANKNAME,NAME_ON_ACC,BRANCH,BALANCE=200.00):
+        self.account_id=ACCOUNT_ID
+        self.bankname=BANKNAME
+        self.name_on_acc=NAME_ON_ACC
+        self.branch=BRANCH
+        self.balance=BALANCE
 
-#     def __str__(self):
-#         return f"Account ID: {self.account_id}, Bank: {self.bankname}, Name: {self.name_on_acc}, Branch: {self.branch}, Balance: {self.balance:.2f}"
+    def __str__(self):
+        return f"Account ID: {self.account_id}, Bank: {self.bankname}, Name: {self.name_on_acc}, Branch: {self.branch}, Balance: {self.balance:.2f}"
 
-# class Bank:
+class Bank:
 
-#     def __init__(self):
-#         self.accounts={}
+    def __init__(self):
+        self.accounts={}
 
-#     def addacc(self):
-#         print("ADD ACCOUNT")
-#         print("Enter the details of the account you wish to add")
+    def addacc(self):
+        print("ADD ACCOUNT")
+        print("Enter the details of the account you wish to add")
 
-#         id=input("Enter a unique ID for your account: ")
-#         if id in self.accounts:
-#             print("Already exists, try again")
+        id=input("Enter a unique ID for your account: ")
+        if id in self.accounts:
+            print("Already exists, try again")
 
-#         bank_name=input("Enter bank name: ")
-#         nameonacc=input("Enter name on the account: ")
-#         branch=input("Enter the branch of your bank: ")
-#         balance=float(input("Enter the initial deposit (minimum = ₹200): "))
+        bank_name=input("Enter bank name: ")
+        nameonacc=input("Enter name on the account: ")
+        branch=input("Enter the branch of your bank: ")
+        balance=float(input("Enter the initial deposit (minimum = ₹200): "))
 
-#         if balance<200:
-#             print("Minimum deposit of ₹200 mandatory")
+        if balance<200:
+            print("Minimum deposit of ₹200 mandatory")
         
-#         new_acc= Account(id, bank_name, nameonacc, branch, balance)
-#         self.accounts[id]= new_acc
-#         print("Account added successfully")
+        new_acc= Account(id, bank_name, nameonacc, branch, balance)
+        self.accounts[id]= new_acc
+        print("Account added successfully")
 
-#     def removeacc(self):
-#         print("REMOVE ACCOUNT")
-#         id=input("Enter the Account ID you wish to delete: ")
-#         if id not in self.accounts:
-#             print("This account does not exist")
-#         else:
-#             del self.accounts[id]
-#             print("Account removed successfully")
+    def removeacc(self):
+        print("REMOVE ACCOUNT")
+        id=input("Enter the Account ID you wish to delete: ")
+        if id not in self.accounts:
+            print("This account does not exist")
+        else:
+            del self.accounts[id]
+            print("Account removed successfully")
 
-#     def withdraw(self):
-#         print("WITHDRAWAL")
-#         id=input("Enter the Account ID from which you would like to withdraw money: ")
+    def withdraw(self):
+        print("WITHDRAWAL")
+        id=input("Enter the Account ID from which you would like to withdraw money: ")
 
-#         if id not in self.accounts:
-#             print("This is account does not exist")
+        if id not in self.accounts:
+            print("This is account does not exist")
         
-#         acc=self.accounts[id]
-#         print("Current balance:",acc.balance)
-#         amount=float(input("Enter the amount you wish to withdraw"))
+        acc=self.accounts[id]
+        print("Current balance:",acc.balance)
+        amount=float(input("Enter the amount you wish to withdraw"))
 
-#         if amount>acc.balance:
-#             print("Insufficient funds")
+        if amount>acc.balance:
+            print("Insufficient funds")
 
-#         acc.balance-=amount
-#         print("Withdrawal successful\nCurrent balance: ",acc.balance)
+        acc.balance-=amount
+        print("Withdrawal successful\nCurrent balance: ",acc.balance)
 
-#     def deposit(self):
-#         print("DEPOSIT")
-#         id=input("Enter the Account ID to which you would like to deposit money: ")
-#         if id not in self.accounts:
-#             print("This is account does not exist")
+    def deposit(self):
+        print("DEPOSIT")
+        id=input("Enter the Account ID to which you would like to deposit money: ")
+        if id not in self.accounts:
+            print("This is account does not exist")
 
-#         acc=self.accounts[id]
-#         print("Current balance:",acc.balance)
-#         dep=float(input("Enter the amount you wish to deposit"))
+        acc=self.accounts[id]
+        print("Current balance:",acc.balance)
+        dep=float(input("Enter the amount you wish to deposit"))
 
-#         acc.balance+=dep
-#         print("Deposit successful\nCurrent balance: ",acc.balance)
+        acc.balance+=dep
+        print("Deposit successful\nCurrent balance: ",acc.balance)
 
-#     def list_of_acc(self):
-#         print("The registered accounts are: ")
-#         for account in self.accounts.values():
-#             print(account)
+    def list_of_acc(self):
+        print("The registered accounts are: ")
+        for account in self.accounts.values():
+            print(account)
 
-# def main():
-#     bank=Bank()
-#     print("THIS IS AN ACCOUNTING PROGRAM")
-#     print("Choose the option given below\n      1.ADD ACCOUNT\n      2.REMOVE ACCOUNT\n      3.WITHDRAWAL\n      4.DEPOSIT\n      5.EXIT")
-#     y=int(input("->"))
+def main():
+    bank=Bank()
+    print("THIS IS AN ACCOUNTING PROGRAM")
+    print("Choose the option given below\n      1.ADD ACCOUNT\n      2.REMOVE ACCOUNT\n      3.WITHDRAWAL\n      4.DEPOSIT\n      5.EXIT")
+    y=int(input("->"))
 
-#     if y==1:
-#         bank.addacc()
-#     elif y==2:
-#         bank.removeacc()
-#     elif y==3:
-#         bank.withdraw()
-#     elif y==4:
-#         bank.deposit()
-#     elif y==5:
-#         exit()
-#     else:
-#         print("invalid input, try again")
-#         main()
+    if y==1:
+        bank.addacc()
+    elif y==2:
+        bank.removeacc()
+    elif y==3:
+        bank.withdraw()
+    elif y==4:
+        bank.deposit()
+    elif y==5:
+        exit()
+    else:
+        print("invalid input, try again")
+        main()
 
-# if __name__=="__main__":
-#     main()      
+if __name__=="__main__":
+    main()      
 
 '''QUESTION 3'''
 
