2b26b4507fdcf5b9a2e27dbe489cd398e0c57557
Nayonika Pal <nayonika.pal@laitkor.com>
2024-07-11 13:12:40 +0530

    json and regex

--- modules.py
+++ modules.py
@@ -6,7 +6,7 @@ import re
 
 """MODULE1"""
 res=m1.greets()
-# print(res)
+print(res)
 
 """MATH MODULE"""
 x,y=5.25,4.01
@@ -14,51 +14,51 @@ a,b=3,2
 o=-8.32
 p,q=121,225
 
-# print(math.ceil(x),math.ceil(y))                          #ceiling func
-# print(math.floor(x),math.floor(y))                        #floor func
-# print(math.comb(a,b))                                     #combination
-# print("MINIMUM:",min(x,y,a,b),"MAXIMUM:",max(x,y,a,b))    #min and max
-# print("ABSOLUTE:",abs(o),abs(x))                          #absolute
-# print(pow(a,b))                                           #power
-# print(math.sqrt(p),math.sqrt(q))                          #square root
-# print(math.pi)                                            #pi
-# print(math.factorial(a))                                  #factorial
-# print(math.gcd(a,b,p,q),math.gcd(a,q))                    #greatest common divisor
-# print(math.lcm(a,b,p,q),math.lcm(a,q))                    #least common multiple
-# print(math.sin(b))                                        #sine func
+print(math.ceil(x),math.ceil(y))                          #ceiling func
+print(math.floor(x),math.floor(y))                        #floor func
+print(math.comb(a,b))                                     #combination
+print("MINIMUM:",min(x,y,a,b),"MAXIMUM:",max(x,y,a,b))    #min and max
+print("ABSOLUTE:",abs(o),abs(x))                          #absolute
+print(pow(a,b))                                           #power
+print(math.sqrt(p),math.sqrt(q))                          #square root
+print(math.pi)                                            #pi
+print(math.factorial(a))                                  #factorial
+print(math.gcd(a,b,p,q),math.gcd(a,q))                    #greatest common divisor
+print(math.lcm(a,b,p,q),math.lcm(a,q))                    #least common multiple
+print(math.sin(b))                                        #sine func
 
 """DATETIME MODULE"""
 t=datetime.datetime.now()
 tm=datetime.datetime(2003,9,26)
-# print(t)                                                  #today's date and time
-# print(t.strftime("%x"))                                   #local version of today's date
-# print(t.strftime("%j"))                                   #day of the year
-# print(t.strftime("%a"))                                   #weekday short version
-# print(t.strftime("%A"))                                   #weekday long version
-# print(tm.strftime("%j"))                                  #set date trial
-# print(t.strftime("%y"))                                   #year short ver
-# print(t.strftime("%Y"))                                   #year long ver
-# print(t.strftime("%H"))                                   #24 hour clock hour
-# print(t.strftime("%I"))                                   #12hr clock hour
-# print(t.strftime("%p"))                                   #am/pm
-# print(t.strftime("%d"))                                   #day of the month
+print(t)                                                  #today's date and time
+print(t.strftime("%x"))                                   #local version of today's date
+print(t.strftime("%j"))                                   #day of the year
+print(t.strftime("%a"))                                   #weekday short version
+print(t.strftime("%A"))                                   #weekday long version
+print(tm.strftime("%j"))                                  #set date trial
+print(t.strftime("%y"))                                   #year short ver
+print(t.strftime("%Y"))                                   #year long ver
+print(t.strftime("%H"))                                   #24 hour clock hour
+print(t.strftime("%I"))                                   #12hr clock hour
+print(t.strftime("%p"))                                   #am/pm
+print(t.strftime("%d"))                                   #day of the month
 
 """JSON MODULE"""
 u={'name':"Nayonika",
    'age':20,
    'city':"jaipur"}
 v=json.dumps(u)
-# print(v)
-# print(json.dumps(42))
-# print(json.dumps(['apple','banana','orange','mango']))
-# print(json.dumps(("apple","banana")))
-# print(json.dumps(True))
-# print(json.dumps(False))
-# print(True)
+print(v)
+print(json.dumps(42))
+print(json.dumps(['apple','banana','orange','mango']))
+print(json.dumps(("apple","banana")))
+print(json.dumps(True))
+print(json.dumps(False))
+print(True)
 
 """REGULAR EXPRESSION MODULE"""
 txt="the quick brown fox jumped over the lazy dog"
-# print(re.findall("the",txt))
-# print(re.search("o",txt))
+print(re.findall("the",txt))
+print(re.search("o",txt))
 f=re.search("the",txt)
 print(f.group())
